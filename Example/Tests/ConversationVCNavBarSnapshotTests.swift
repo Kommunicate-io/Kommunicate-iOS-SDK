8c5f30090574652b5447ae1fc70c803891e840a8
shivampokhriyal <shivampokhriyal1997@gmail.com>
2018-11-20 15:32:13 +0530

    Add test cases for navigation bar in Conversation screen

---
+++ Example/Tests/ConversationVCNavBarSnapshotTests.swift
@@ -0,0 +1,56 @@
+//
+//  ConversationVCNavBarSnapshotTests.swift
+//  Kommunicate_Tests
+//
+//  Created by Shivam Pokhriyal on 20/11/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Nimble_Snapshots
+import Applozic
+@testable import Kommunicate
+
+class ConversationVCNavBarSnapshotTests: QuickSpec, NavigationBarCallbacks {
+    func backButtonPressed() {
+    
+    }
+    
+    let mockContact: ALContact = {
+        let alContact = ALContact()
+        alContact.userId = "demoUserId"
+        alContact.displayName = "Demo Display Name"
+        return alContact
+    }()
+    
+    override func spec() {
+        describe ("conversationVC NavBar") {
+            var navigationController: UINavigationController!
+            var customNavigationView: ConversationVCNavBar!
+            var viewController: UIViewController!
+            
+            beforeEach {
+                customNavigationView = ConversationVCNavBar(navigationBarBackgroundColor: UIColor.navigationOceanBlue(), delegate: self)
+                viewController = UIViewController(nibName: nil, bundle: nil)
+                viewController.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: customNavigationView)
+                navigationController = UINavigationController(rootViewController: viewController)
+            }
+            
+            it ("show agent online") {
+                self.mockContact.connected = true
+                customNavigationView.updateView(assignee: self.mockContact)
+                navigationController.navigationBar.snapshotView(afterScreenUpdates: true)
+                expect(navigationController.navigationBar).to(haveValidSnapshot())
+            }
+            
+            it ("show agent offline") {
+                self.mockContact.connected = false
+                customNavigationView.updateView(assignee: self.mockContact)
+                navigationController.navigationBar.snapshotView(afterScreenUpdates: true)
+                expect(navigationController.navigationBar).to(haveValidSnapshot())
+            }
+        }
+    }
+    
+}
