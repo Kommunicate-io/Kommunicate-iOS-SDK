e0c904ef4ffe0703c357a13edb483dd96ded1d2f
Mukesh <mukesh9039@gmail.com>
2019-03-07 15:46:29 +0530

    Login: Update view's frame when keyboard is visible

--- Example/Kommunicate/LoginViewController.swift
+++ Example/Kommunicate/LoginViewController.swift
@@ -11,6 +11,7 @@ import Kommunicate
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var userName: UITextField!
     @IBOutlet weak var password: UITextField!
 
@@ -19,14 +20,22 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var loginAsVisitorButton: UIButton!
 
+    // To avoid scrolling up multiple times when a different text field is active.
+    var isKeyboardVisible = false
+    var originalContentInset: UIEdgeInsets = .zero
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         loginAsVisitorButton.layer.borderWidth = 1
         loginAsVisitorButton.layer.borderColor = UIColor(hexString: "1588B2")?.cgColor
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
     @IBAction func getStartedBtn(_ sender: AnyObject) {
+        resignFields()
         let applicationId = AppDelegate.appId
         setupApplicationKey(applicationId)
 
@@ -42,7 +51,7 @@ class LoginViewController: UIViewController {
         }
         let kmUser = userWithUserId(userIdEntered, andApplicationId: applicationId)
 
-        print("userName:: " , kmUser.userId)
+        print("userId:: ", kmUser.userId)
         if(!((emailId.text?.isEmpty)!)){
             kmUser.email = emailId.text
         }
@@ -54,11 +63,51 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func loginAsVisitor(_ sender: Any) {
+        resignFields()
         let applicationId = AppDelegate.appId
         setupApplicationKey(applicationId)
 
         let kmUser = userWithUserId(Kommunicate.randomId(), andApplicationId: applicationId)
         registerUser(kmUser)
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+
+        guard isKeyboardVisible,
+            let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval
+            else { return }
+        isKeyboardVisible = false
+        UIView.animate(withDuration: duration, delay: 0, options: [.beginFromCurrentState], animations: {
+            self.scrollView.contentInset = self.originalContentInset
+            self.scrollView.scrollIndicatorInsets = self.originalContentInset
+            self.scrollView.contentOffset = CGPoint(x: 0, y: 0)
+        })
+    }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+
+        guard let frame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
+            let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
+        var inset = scrollView.contentInset
+        originalContentInset = inset
+
+        // This will convert the rect. w.r.t to the current view.
+        let converted = self.view.convert(frame, from: nil)
+        let intersection = converted.intersection(frame)
+        // This will give us the value of how much to move up.
+        var bottomInset = intersection.height
+        if #available(iOS 11.0, *) {
+             bottomInset = bottomInset - self.view.safeAreaInsets.bottom
+        }
+
+        inset.bottom = bottomInset
+        guard !isKeyboardVisible else { return }
+        isKeyboardVisible = true
+        UIView.animate(withDuration: duration, delay: 0, options: [.beginFromCurrentState], animations: {
+            self.scrollView.contentInset = inset
+            self.scrollView.scrollIndicatorInsets = inset
+            self.scrollView.contentOffset = CGPoint(x: 0, y: bottomInset - 60)
+        })
     }
 
     private func setupApplicationKey(_ applicationId: String) {
@@ -83,14 +132,20 @@ class LoginViewController: UIViewController {
             response, error in
             self.activityIndicator.stopAnimating()
             guard error == nil else {
-                NSLog("[REGISTRATION] Kommunicate user registration error: %@", error.debugDescription)
+                print("[REGISTRATION] Kommunicate user registration error: %@", error.debugDescription)
                 return
             }
-            NSLog("User registration was successful: %@ \(String(describing: response?.isRegisteredSuccessfully()))")
+            print("User registration was successful: %@ \(String(describing: response?.isRegisteredSuccessfully()))")
             if let viewController = UIStoryboard(name: "Main", bundle: nil)
                 .instantiateViewController(withIdentifier: "NavViewController") as? UINavigationController {
                 self.present(viewController, animated:true, completion: nil)
             }
         })
+    }
+
+    private func resignFields() {
+        userName.resignFirstResponder()
+        emailId.resignFirstResponder()
+        password.resignFirstResponder()
     }
 }
