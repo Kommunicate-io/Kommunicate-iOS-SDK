a52a6b3a7f92abef593acb040cf34c8283a67b62
ShivamPokhriyal <shivampokhriyal1997@gmail.com>
2019-08-26 13:18:43 +0530

    Fix notification tap issue

---
+++ Kommunicate/Classes/KMPushNotificationHandler.swift
@@ -0,0 +1,114 @@
+//
+//  KMPushNotificationHandler.swift
+//  Kommunicate
+//
+//  Created by Shivam Pokhriyal on 23/08/19.
+//
+
+import Foundation
+import Applozic
+import ApplozicSwift
+
+public class KMPushNotificationHandler: Localizable {
+    public static let shared = KMPushNotificationHandler()
+    var navVC: UINavigationController?
+
+    var contactId: String?
+    var groupId: NSNumber?
+    var conversationId: NSNumber?
+    var configuration: KMConfiguration!
+
+    /// Make it true to hide chat list on press of notification
+    public static var hideChatListOnNotification: Bool = false
+
+    private var alContact: ALContact? {
+        let alContactDbService = ALContactDBService()
+        guard let alContact = alContactDbService.loadContact(byKey: "userId", value: self.contactId) else {
+            return nil
+        }
+        return alContact
+    }
+
+    private var alChannel: ALChannel? {
+        let alChannelService = ALChannelService()
+
+        // TODO:  This is a workaround as other method uses closure.
+        // Later replace this with:
+        // alChannelService.getChannelInformation(, orClientChannelKey: , withCompletion: )
+        guard let alChannel = alChannelService.getChannelByKey(self.groupId) else {
+            return nil
+        }
+        return alChannel
+    }
+
+    public func dataConnectionNotificationHandlerWith(_ configuration: KMConfiguration) {
+
+        self.configuration = configuration
+
+        // No need to add removeObserver() as it is present in pushAssist.
+        NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "showNotificationAndLaunchChat"), object: nil, queue: nil, using: {[weak self] notification in
+            print("launch chat push notification received")
+            self?.contactId = nil
+            self?.groupId = nil
+            self?.conversationId = nil
+            //Todo: Handle group
+
+            guard let weakSelf = self, let object = notification.object as? String else { return }
+            let components = object.components(separatedBy: ":")
+
+            if components.count > 2 {
+                guard let componentElement = Int(components[1]) else { return }
+                let id = NSNumber(integerLiteral: componentElement)
+                weakSelf.groupId = id
+            } else if components.count == 2 {
+                guard let conversationComponent = Int(components[1]) else { return }
+                weakSelf.conversationId = NSNumber(integerLiteral: conversationComponent)
+                weakSelf.contactId = components[0]
+            } else {
+                weakSelf.contactId = object
+            }
+
+            guard let userInfo = notification.userInfo as? [String: Any], let state = userInfo["updateUI"] as? NSNumber else { return }
+
+            switch state {
+            case NSNumber(value: APP_STATE_ACTIVE.rawValue):
+                guard let userInfo = notification.userInfo, let alertValue = userInfo["alertValue"] as? String else {
+                    return
+                }
+                ///TODO: FIX HERE. USE conversationId also.
+                ALUtilityClass.thirdDisplayNotificationTS(alertValue, andForContactId: weakSelf.contactId, withGroupId: weakSelf.groupId, completionHandler: {
+
+                    _ in
+                    weakSelf.notificationTapped(userId: weakSelf.contactId, groupId: weakSelf.groupId)
+
+                })
+            default:
+                weakSelf.launchIndividualChatWith(userId: weakSelf.contactId, groupId: weakSelf.groupId)
+            }
+        })
+    }
+
+    func launchIndividualChatWith(userId: String?, groupId: NSNumber?) {
+        NSLog("Called via notification and user id is: ", userId ?? "Not Present")
+        guard let topVC = ALPushAssist().topViewController, let groupId = self.groupId else { return }
+
+        if (KMPushNotificationHandler.hideChatListOnNotification) {
+            Kommunicate.openChatWith(groupId: groupId, from: topVC) { result in
+                print("Launch conversation from notification result :: \(result)")
+            }
+            return
+        }
+
+        let notificationData = NotificationHelper.NotificationData(userId: userId, groupId: groupId, conversationId: conversationId)
+        let vc = NotificationHelper().getConversationVCToLaunch(notification: notificationData, configuration: configuration)
+        Kommunicate.configureListVC(vc)
+        let nav = ALKBaseNavigationViewController(rootViewController: vc)
+        navVC?.modalTransitionStyle = .crossDissolve
+        topVC.present(nav, animated: true, completion: nil)
+    }
+
+    func notificationTapped(userId: String?, groupId: NSNumber?) {
+        launchIndividualChatWith(userId: userId, groupId: groupId)
+    }
+
+}
--- Kommunicate/Classes/Kommunicate.swift
+++ Kommunicate/Classes/Kommunicate.swift
@@ -23,7 +23,6 @@ public typealias KMPushNotificationService = ALPushNotificationService
 public typealias KMAppLocalNotification = ALAppLocalNotifications
 public typealias KMDbHandler = ALDBHandler
 public typealias KMRegisterUserClientService = ALRegisterUserClientService
-public typealias KMPushNotificationHandler = ALKPushNotificationHandler
 public typealias KMConfiguration = ALKConfiguration
 let conversationCreateIdentifier = 112233445
 let faqIdentifier =  11223346
@@ -207,19 +206,23 @@ open class Kommunicate: NSObject,Localizable{
     /// - Returns: Instance of `ALKConversationListViewController`
     @objc open class func conversationListViewController() -> ALKConversationListViewController {
         let conversationVC = ALKConversationListViewController(configuration: Kommunicate.defaultConfiguration)
-        conversationVC.conversationListTableViewController.dataSource.cellConfigurator = {
+        configureListVC(conversationVC)
+        return conversationVC
+    }
+
+    open class func configureListVC(_ vc: ALKConversationListViewController) {
+        vc.conversationListTableViewController.dataSource.cellConfigurator = {
             (messageModel, tableCell) in
             let cell = tableCell as! ALKChatCell
             let message = ChatMessage(message: messageModel)
             cell.update(viewModel: message, identity: nil, disableSwipe: Kommunicate.defaultConfiguration.disableSwipeInChatCell)
-            cell.chatCellDelegate = conversationVC.conversationListTableViewController.self
+            cell.chatCellDelegate = vc.conversationListTableViewController.self
         }
         let conversationViewController = KMConversationViewController(configuration: Kommunicate.defaultConfiguration)
         conversationViewController.kmConversationViewConfiguration = kmConversationViewConfiguration
         conversationViewController.viewModel = ALKConversationViewModel(contactId: nil, channelKey: nil, localizedStringFileName: defaultConfiguration.localizedStringFileName)
-        conversationVC.conversationViewController = conversationViewController
+        vc.conversationViewController = conversationViewController
         observeListControllerNavigationCustomButtonClick()
-        return conversationVC
     }
 
     /**
@@ -251,19 +254,24 @@ open class Kommunicate: NSObject,Localizable{
                 completionHandler(false)
                 return
             }
-            let convViewModel = ALKConversationViewModel(contactId: nil, channelKey: key, localizedStringFileName: defaultConfiguration.localizedStringFileName)
-            let conversationViewController = KMConversationViewController(configuration: Kommunicate.defaultConfiguration)
-            conversationViewController.title = channel.name
-            conversationViewController.viewModel = convViewModel
-            conversationViewController.kmConversationViewConfiguration = kmConversationViewConfiguration
-            if let navigationVC = viewController.navigationController {
-                navigationVC.pushViewController(conversationViewController, animated: false)
-            } else {
-                let navigationController = UINavigationController(rootViewController: conversationViewController)
-                viewController.present(navigationController, animated: false, completion: nil)
-            }
-            completionHandler(true)
+            self.openChatWith(groupId: key, from: viewController, completionHandler: { result in
+                completionHandler(result)
+            })
         }
+    }
+
+    class func openChatWith(groupId: NSNumber, from viewController: UIViewController, completionHandler: @escaping (Bool) -> Void) {
+        let convViewModel = ALKConversationViewModel(contactId: nil, channelKey: groupId, localizedStringFileName: defaultConfiguration.localizedStringFileName)
+        let conversationViewController = KMConversationViewController(configuration: Kommunicate.defaultConfiguration)
+        conversationViewController.viewModel = convViewModel
+        conversationViewController.kmConversationViewConfiguration = kmConversationViewConfiguration
+        if let navigationVC = viewController.navigationController {
+            navigationVC.pushViewController(conversationViewController, animated: false)
+        } else {
+            let navigationController = UINavigationController(rootViewController: conversationViewController)
+            viewController.present(navigationController, animated: false, completion: nil)
+        }
+        completionHandler(true)
     }
 
     /**
