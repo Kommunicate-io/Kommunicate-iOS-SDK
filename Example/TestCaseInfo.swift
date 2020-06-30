5494ba05fe8fe0e212ec50d82776dffc62b79361
archit jain <archit2602@gmail.com>
2020-06-30 18:54:04 +0530

    UI Test Cases and Enum file

---
+++ Example/TestCaseInfo.swift
@@ -0,0 +1,61 @@
+//
+//  Constante.swift
+//  ApplozicSwiftDemoUITests
+//
+//  Created by Kommunicate on 26/12/19.
+//  Copyright © 2019 Applozic. All rights reserved.
+//
+
+import Foundation
+
+enum AppPermission {
+    enum AlertMessage {
+        static let accessNotificationInApplication = "“KommunicateDemo” Would Like to Send You Notifications"
+        static let accessPhoto = "“KommunicateDemo” Would Like to Access Your Photos"
+        static let accessContact = "“KommunicateDemo” Would Like to Access Your Contacts"
+        static let accessLocation = "“KommunicateDemo” to access your location while you are using the app?"
+    }
+    
+    enum AlertButton {
+        static let allow = "Allow"
+        static let ok = "OK"
+        static let allowLoation = "Allow While Using App"
+    }
+}
+
+enum InAppButton {
+    enum LaunchScreen {
+        static let getStarted = "Get Started"
+        static let launchChat = "Launch Chat"
+    }
+    
+    enum CreatingGroup {
+        static let startNewIcon = "startNewIcon"
+    }
+    
+    enum EditGroup {
+        static let launch = "Launch Conversations"
+        static let iconSendWhite = "icon send white"
+    }
+    
+    enum ConversationScreen {
+        static let openCamera = "photoButtonInConversationScreen"
+        static let send = "sendButton"
+        static let openPhotos = "galleryButtonInConversationScreen"
+        static let selectPhoto = "Photos"
+        static let openLocation = "locationButtonInConversationScreen"
+        static let sendLocation = "Send Location"
+        static let done = "Done"
+    }
+}
+
+enum AppScreen {
+    static let myChatScreen = "My Chats"
+    static let chatBar = "chatBar"
+}
+
+enum AppTextFeild {
+    static let userId = "User id (Use a random id for the first time login)"
+    static let password = "Password"
+    static let chatTextView = "chatTextView"
+}
