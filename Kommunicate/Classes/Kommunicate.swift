cf77c328301bf5057271670668527c32e0c857c6
mukeshthawani <mukesh9039@gmail.com>
2020-11-11 12:44:21 +0530

    Add an option to show conversation list on back tap

--- Kommunicate/Classes/Kommunicate.swift
+++ Kommunicate/Classes/Kommunicate.swift
@@ -281,23 +281,31 @@ open class Kommunicate: NSObject,Localizable{
      - clientGroupId: clientChannelKey of the Group.
      - viewController: ViewController from which the group chat will be launched.
      - prefilledMessage: Prefilled message for chatbox.
+     - showListOnBack: If true, then the conversation list will be shown on tap of the back button,
      - completionHandler: Called with the information whether the conversation was
      shown or not.
 
      */
-    @objc open class func showConversationWith(groupId clientGroupId: String,
-                                               from viewController: UIViewController,
-                                               prefilledMessage: String? = nil,
-                                               completionHandler: @escaping (Bool) -> Void) {
+    @objc open class func showConversationWith(
+        groupId clientGroupId: String,
+        from viewController: UIViewController,
+        prefilledMessage: String? = nil,
+        showListOnBack: Bool = false,
+        completionHandler: @escaping (Bool) -> Void
+    ) {
         let alChannelService = ALChannelService()
         alChannelService.getChannelInformation(nil, orClientChannelKey: clientGroupId) { (channel) in
             guard let channel = channel, let key = channel.key else {
                 completionHandler(false)
                 return
             }
-            self.openChatWith(groupId: key, from: viewController, prefilledMessage: prefilledMessage, completionHandler: { result in
+            self.openChatWith(
+                groupId: key,
+                from: viewController,
+                prefilledMessage: prefilledMessage,
+                showListOnBack: showListOnBack) { result in
                 completionHandler(result)
-            })
+            }
         }
     }
 
@@ -401,17 +409,31 @@ open class Kommunicate: NSObject,Localizable{
         vc.conversationViewController = conversationViewController
     }
 
-    class func openChatWith(groupId: NSNumber,
-                            from viewController: UIViewController,
-                            prefilledMessage: String? = nil,
-                            completionHandler: @escaping (Bool) -> Void) {
-        let convViewModel = ALKConversationViewModel(contactId: nil, channelKey: groupId, localizedStringFileName: defaultConfiguration.localizedStringFileName, prefilledMessage: prefilledMessage)
-        let conversationViewController = KMConversationViewController(configuration: Kommunicate.defaultConfiguration, conversationViewConfiguration: kmConversationViewConfiguration)
-        conversationViewController.viewModel = convViewModel
-        let navigationController = KMBaseNavigationViewController(rootViewController: conversationViewController)
-        navigationController.modalPresentationStyle = .fullScreen
-        viewController.present(navigationController, animated: true, completion: nil)
-        completionHandler(true)
+    class func openChatWith(
+        groupId: NSNumber,
+        from viewController: UIViewController,
+        prefilledMessage: String? = nil,
+        showListOnBack: Bool = false,
+        completionHandler: @escaping (Bool) -> Void
+    ) {
+        if showListOnBack {
+            let conversationVC = conversationListViewController()
+            conversationVC.channelKey = groupId
+            let navVC = KMBaseNavigationViewController(rootViewController: conversationVC)
+            navVC.modalPresentationStyle = .fullScreen
+            viewController.present(navVC, animated: false) {
+                completionHandler(true)
+            }
+        } else {
+            let convViewModel = ALKConversationViewModel(contactId: nil, channelKey: groupId, localizedStringFileName: defaultConfiguration.localizedStringFileName, prefilledMessage: prefilledMessage)
+            let conversationViewController = KMConversationViewController(configuration: Kommunicate.defaultConfiguration, conversationViewConfiguration: kmConversationViewConfiguration)
+            conversationViewController.viewModel = convViewModel
+            let navigationController = KMBaseNavigationViewController(rootViewController: conversationViewController)
+            navigationController.modalPresentationStyle = .fullScreen
+            viewController.present(navigationController, animated: false) {
+                completionHandler(true)
+            }
+        }
     }
 
     //MARK: - Private methods
