51c37c711a13afec3b1897df612d2b982fdf72de
Kirti S <shilwantkirti@gmail.com>
2021-04-05 14:12:20 +0530

    Add .build and .swiftpm to .gitignore and remove unrequired files

--- .build/x86_64-apple-macosx/debug/ApplozicCore.build/DerivedSources/resource_bundle_accessor.h
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-
-NSBundle* ApplozicCore_SWIFTPM_MODULE_BUNDLE(void);
-
-#define SWIFTPM_MODULE_BUNDLE ApplozicCore_SWIFTPM_MODULE_BUNDLE()
--- .build/x86_64-apple-macosx/debug/ApplozicCore.build/DerivedSources/resource_bundle_accessor.m
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-
-NSBundle* ApplozicCore_SWIFTPM_MODULE_BUNDLE() {
-    NSURL *bundleURL = [[[NSBundle mainBundle] bundleURL] URLByAppendingPathComponent:@"Applozic_ApplozicCore.bundle"];
-    return [NSBundle bundleWithURL:bundleURL];
-}
