dd31fda2aa59ecc1efd918f89f649f6121288e0d
Kirti S <shilwantkirti@gmail.com>
2021-04-06 01:43:06 +0530

    Add review changes to Package.swift for Kommunicate

--- Package.swift
+++ Package.swift
@@ -1,33 +1,25 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Kommunicate-iOS-SDK",
+    name: "Kommunicate",
     defaultLocalization: "en",
     platforms: [.iOS(.v10)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Kommunicate-iOS-SDK",
-            targets: ["Kommunicate-iOS-SDK"]),
+            name: "Kommunicate",
+            targets: ["Kommunicate"]),
     ],
     dependencies: [
         .package(name: "ApplozicSwift", url: "https://github.com/AppLozic/ApplozicSwift.git", .branch("SPM-Support-changes")),
+        .package(name: "Kingfisher", url: "https://github.com/onevcat/Kingfisher.git", "5.14.0" ..< "5.15.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Kommunicate-iOS-SDK",
-            dependencies: [],
-            path: "Example/Kommunicate"
-        ),
-        .testTarget(
-            name: "Kommunicate-iOS-SDKTests",
-            dependencies: ["Kommunicate-iOS-SDK"],
-            path: "Example/Tests"
+            name: "Kommunicate",
+            dependencies: [.product(name: "ApplozicSwift", package: "ApplozicSwift")],
+            path: "Kommunicate"
         ),
     ]
 )
