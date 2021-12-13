d7ae01918da6c7aa4b02decfe9fadff920de6640
Kirti Shilwant <shilwantkirti@gmail.com>
2021-12-13 22:54:48 +0530

    Add pre-chat code in loginAsVisitor()

--- Example/Kommunicate/LoginViewController.swift
+++ Example/Kommunicate/LoginViewController.swift
@@ -34,6 +34,20 @@ class LoginViewController: UIViewController {
 
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
+    
+    //to log out from prechat
+    override func viewDidAppear(_ animated: Bool) {
+        if Kommunicate.isLoggedIn {
+          Kommunicate.logoutUser { (result) in
+            switch result {
+            case .success(_):
+              print("Logout success")
+            case .failure( _):
+              print("Logout failure, now registering remote notifications(if not registered)")
+            }
+          }
+        }
+      }
 
     @IBAction func getStartedBtn(_ sender: AnyObject) {
         resignFields()
@@ -64,12 +78,16 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func loginAsVisitor(_ sender: Any) {
-        resignFields()
-        let applicationId = (UIApplication.shared.delegate as! AppDelegate).appId
-        setupApplicationKey(applicationId)
-
-        let kmUser = userWithUserId(Kommunicate.randomId(), andApplicationId: applicationId)
-        registerUser(kmUser)
+//        resignFields()
+//        let applicationId = (UIApplication.shared.delegate as! AppDelegate).appId
+//        setupApplicationKey(applicationId)
+//
+//        let kmUser = userWithUserId(Kommunicate.randomId(), andApplicationId: applicationId)
+//        registerUser(kmUser)
+        let sampleConversationBuilder = KMConversationBuilder().useLastConversation(false).build()
+        Kommunicate.createConversationWithPreChat(appID: (UIApplication.shared.delegate as! AppDelegate).appId, conversation: nil, viewController: self) { error in
+            print(error as Any)
+        }
     }
 
     @objc func keyboardWillHide(notification: NSNotification) {
