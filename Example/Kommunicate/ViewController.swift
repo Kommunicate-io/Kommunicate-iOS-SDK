0ba93f13b3e2858546884e95082692877a4532ef
Sathyan Elangovan <sathya576@gmail.com>
2022-01-17 11:23:43 +0530

    removed lines which helped for testing

--- Example/Kommunicate/ViewController.swift
+++ Example/Kommunicate/ViewController.swift
@@ -12,7 +12,7 @@ import Kommunicate
 
 class ViewController: UIViewController {
     let activityIndicator = UIActivityIndicatorView(style: .gray)
-let teamid = "67476167"
+
     override func viewDidLoad() {
         super.viewDidLoad()
         activityIndicator.center = CGPoint(x: view.bounds.size.width/2,
@@ -20,62 +20,21 @@ let teamid = "67476167"
         view.addSubview(activityIndicator)
         view.bringSubviewToFront(activityIndicator)
     }
-// I have changed the launchConversation method for testing purpose, Will remove all the changes after
+
     @IBAction func launchConversation(_ sender: Any) {
         activityIndicator.startAnimating()
         view.isUserInteractionEnabled = false
         
-        let kommunicateConversationBuilder = KMConversationBuilder()
-            .useLastConversation(true)
-        let conversation = kommunicateConversationBuilder.build()
-        let teamid = "67476167"
-
-        Kommunicate.createConversation(conversation: conversation) { (result) in
-            switch result {
-            case .success(let conversationId):
-                self.activityIndicator.stopAnimating()
-                self.view.isUserInteractionEnabled = true
-                DispatchQueue.main.async {
-                    Kommunicate.showConversationWith(groupId: conversationId, from: self, completionHandler: { success in
-                        guard success else {
-                            print("Failed to create show conversation")
-//                            completion(KommunicateError.conversationNotPresent)
-                            return
-                        }
-                        let teamid = "67476167"
-
-//
-                        Kommunicate.updateTeamId(conversation: conversation, teamId: teamid){ (result) in
-                            switch result {
-                            case .success(let groupId):
-                                print("Successfully to udpated the team id \(groupId)")
-
-                            case.failure(_):
-                                print("Failed to udpate the team id")
-                            }
-
-                        }
-//
-                        print("Kommunicate: conversation was shown")
-//                        completion(nil)
-                    })
-                }
-            case .failure(_):
-//                completion(KommunicateError.conversationCreateFailed)
-                return
+        Kommunicate.createAndShowConversation(from: self, completion: {
+            error in
+            self.activityIndicator.stopAnimating()
+            self.view.isUserInteractionEnabled = true
+            if error != nil {
+                print("Error while launching")
             }
-        }
-      
-//
-//        Kommunicate.createAndShowConversation(from: self, completion: {
-//            error in
-//            self.activityIndicator.stopAnimating()
-//            self.view.isUserInteractionEnabled = true
-//            if error != nil {
-//                print("Error while launching")
-//            }
-//        })
+        })
     }
+    
     @IBAction func logoutAction(_ sender: Any) {
         Kommunicate.logoutUser { (result) in
             switch result {
