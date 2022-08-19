74d677eacf8c66f0d732c295a79daf3c02c36760
Sathyan Elangovan <sathya576@gmail.com>
2022-08-19 15:07:25 +0530

    Added embedding sdk flow

---
+++ Example/Kommunicate/EmbeddedViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EmbeddedViewController.swift
+//  Kommunicate_Example
+//
+//  Created by sathyan elangovan on 18/08/22.
+//  Copyright © 2022 CocoaPods. All rights reserved.
+//
+
+import Foundation
