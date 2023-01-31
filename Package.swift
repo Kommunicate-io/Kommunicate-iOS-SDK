b8c8555b3faa8e42e9e1ad72f90b269a1d8fd469
Sathyan-KM <sathyan.elangovan@kommunicate.io>
2023-01-31 21:35:04 +0530

    upgraded KM Chat ui to 0.2.9

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "KommunicateChatUI-iOS-SDK", url: "https://github.com/Kommunicate-io/KommunicateChatUI-iOS-SDK.git", from: "0.2.8"),
+        .package(name: "KommunicateChatUI-iOS-SDK", url: "https://github.com/Kommunicate-io/KommunicateChatUI-iOS-SDK.git", from: "0.2.9"),
           ],
     targets: [
         .target(
