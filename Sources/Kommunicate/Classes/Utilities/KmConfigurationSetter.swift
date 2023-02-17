3b137c52f3db237676d774911a520db82352abb9
sathyan.elangovan@kommunicate.io <sathyan.elangovan@kommunicate.io>
2023-02-17 18:55:57 +0530

    added customisation from string for restart conversation, rate conversation, start new conversation button,

--- Sources/Kommunicate/Classes/Utilities/KmConfigurationSetter.swift
+++ Sources/Kommunicate/Classes/Utilities/KmConfigurationSetter.swift
@@ -96,7 +96,25 @@ public class KMConfigurationSetter {
             if let restrictedWordRegex = settingDict["restrictedWordRegex"] as? String, !restrictedWordRegex.isEmpty {
                 Kommunicate.defaultConfiguration.restrictedMessageRegexPattern = restrictedWordRegex
             }
+            
+            if let rateConversationMenuOption = settingDict["rateConversationMenuOption"] as? Bool {
+                Kommunicate.defaultConfiguration.rateConversationMenuOption = rateConversationMenuOption
+            }
+            
+            if let hideRestartConversationButton = settingDict["restartConversationButtonVisibility"] as? Bool {
+                Kommunicate.defaultConfiguration.hideRestartConversationButton = hideRestartConversationButton
+            }
+            
+            if let enableBackArrowOnConversationListScreen = settingDict["enableBackArrowOnConversationListScreen"] as? Bool {
+                Kommunicate.defaultConfiguration.enableBackArrowOnConversationListScreen = enableBackArrowOnConversationListScreen
+            }
+            
+            if let hideBottomStartNewConversationButton = settingDict["hideBottomStartNewConversationButton"] as? Bool {
+                Kommunicate.defaultConfiguration.hideBottomStartNewConversationButton = hideBottomStartNewConversationButton
+            }
+            
         } catch let error as NSError {
+            print("Failed to read setting json string \(error.description)")
             return false
         }
         return true
