4abe69b9dc0418090f3b19941f8c7f5d6a36a731
sathyan.elangovan@kommunicate.io <sathyan.elangovan@kommunicate.io>
2023-03-10 13:01:33 +0530

    updated pod & package swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     //a97bd3ede123f281f4b913cbe1c3fff9cc634a20
     dependencies: [
 //        .package(name: "KommunicateChatUI-iOS-SDK", url: "https://github.com/Kommunicate-io/KommunicateChatUI-iOS-SDK.git", from: "1.0.0"),
-        .package(url:"https://github.com/Sathyan-KM/KommunicateChatUI-iOS-SDK.git",.revisionItem("a97bd3ede123f281f4b913cbe1c3fff9cc634a20"))
+        .package(url:"https://github.com/Sathyan-KM/KommunicateChatUI-iOS-SDK.git",.revisionItem("963d103db0b3d17063d2de04bf69b1b3a5c5d884"))
           ],
     targets: [
         .target(
