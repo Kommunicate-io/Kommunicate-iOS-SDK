d4bc3f4f78163c4205dfbe47c6eff1c2eb769fe6
sathyan.elangovan@kommunicate.io <sathyan.elangovan@kommunicate.io>
2023-03-10 16:53:32 +0530

    upgraded KM Chat ui

--- Package.swift
+++ Package.swift
@@ -12,10 +12,8 @@ let package = Package(
             targets: ["Kommunicate"]
         ),
     ],
-    //a97bd3ede123f281f4b913cbe1c3fff9cc634a20
     dependencies: [
-//        .package(name: "KommunicateChatUI-iOS-SDK", url: "https://github.com/Kommunicate-io/KommunicateChatUI-iOS-SDK.git", from: "1.0.0"),
-        .package(url:"https://github.com/Sathyan-KM/KommunicateChatUI-iOS-SDK.git",.revisionItem("963d103db0b3d17063d2de04bf69b1b3a5c5d884"))
+        .package(name: "KommunicateChatUI-iOS-SDK", url: "https://github.com/Kommunicate-io/KommunicateChatUI-iOS-SDK.git", from: "1.0.1"),
           ],
     targets: [
         .target(
