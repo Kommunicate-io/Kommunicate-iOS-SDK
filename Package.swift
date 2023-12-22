90449ffb487d59333dc1e11a55700691d7cb6437
Abhijeet Ranjan <139110221+AbhijeetRanjan308@users.noreply.github.com>
2023-12-22 20:26:18 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-            .package(name: "KommunicateChatUI-iOS-SDK", url: "https://github.com/Kommunicate-io/KommunicateChatUI-iOS-SDK.git", from: "1.2.4"),
+            .package(url: "https://github.com/Kommunicate-io/KommunicateChatUI-iOS-SDK.git", .branch(SPM_TEST)),
     ],
     targets: [
         .target(
