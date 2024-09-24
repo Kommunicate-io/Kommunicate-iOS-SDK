c1c181bb291a9ae494716478716febacfa758a87
Abhijeet Ranjan <abhijeet.ranjan@kommunicate.io>
2024-09-24 18:11:18 +0530

    Updated the code for automation testing.

--- Example/Kommunicate_ExampleUITests/KommunicateRichMessageUITests.swift
+++ Example/Kommunicate_ExampleUITests/KommunicateRichMessageUITests.swift
@@ -18,9 +18,9 @@ class KommunicateRichMessageUITests: XCTestCase {
         static let typeText6 = "List Template"
         static let typeText7 = "Single card"
         static let typeText8 = "Card Carousel"
-        static let AppId = "TestAppId"
-        static let fillUserId = "TestUserId"
-        static let fillPassword = "TestUserPassword"
+        static let AppId = loginCreadentials.testAppID
+        static let fillUserId = loginCreadentials.userID
+        static let fillPassword = loginCreadentials.password
     }
 
     override func setUp() {
@@ -34,7 +34,7 @@ class KommunicateRichMessageUITests: XCTestCase {
         }
         let app = XCUIApplication()
         if let appId = appIdFromEnvVars() {
-            app.launchArguments = ["-appId", appId]
+            app.launchArguments = [GroupData.AppId, appId]
         }
         app.launch()
         sleep(5)
@@ -45,21 +45,34 @@ class KommunicateRichMessageUITests: XCTestCase {
     }
 
     func testSuggestedRepliesTemplate() {
-       let app = beforeTest_Launch_NewConversation()
-       waitFor(object: app) { $0.exists }
-       app.typeText(GroupData.typeText1) // typing message
-       app.buttons[InAppButton.ConversationScreen.send].tap() // sending message in group
-       let suggestedReplyFirstResponse = app.tables[AppScreen.innerChatScreenTableView]
-           .textViews[RichMessageResponseText.sugggestedRepliesFirstMessage]
-       waitFor(object: suggestedReplyFirstResponse) { $0.exists }
-       guard app.tables[AppScreen.innerChatScreenTableView].staticTexts[RichMessageButtons.button].isEnabled else {
-           XCTFail("Button in Suggested Replies Template is disabled or not visible")
-           return
-       }
-       app.tables[AppScreen.innerChatScreenTableView].staticTexts[RichMessageButtons.button].tap()
-       let suggestedRepliesResponse = app.tables[AppScreen.innerChatScreenTableView]
-           .textViews[RichMessageResponseText.suggestedButtonResponse]
-       waitFor(object: suggestedRepliesResponse) { $0.exists }
+        let app = beforeTest_Launch_NewConversation()
+        waitFor(object: app) { $0.exists }
+        app.typeText(GroupData.typeText1) // typing message
+        app.buttons[InAppButton.ConversationScreen.send].tap() // sending message in group
+        sleep(3) /// To wait for response
+        app.swipeUp() /// To get to bottom of the screen.
+        let suggestedReplyFirstResponse = app.tables[AppScreen.innerChatScreenTableView]
+            .textViews[RichMessageResponseText.sugggestedRepliesFirstMessage]
+        waitFor(object: suggestedReplyFirstResponse) { $0.exists }
+        let buttons = app.tables[AppScreen.innerChatScreenTableView].staticTexts.matching(identifier: RichMessageButtons.button)
+            
+        // Ensure there's at least one element
+        guard buttons.count > 0 else {
+            XCTFail("No buttons found in Suggested Replies Template")
+            return
+        }
+            
+        // Use element(boundBy:) to select a specific button, e.g., the first one
+        let specificButton = buttons.element(boundBy: 0) // Change index as needed
+            
+        guard specificButton.isEnabled else {
+            XCTFail("Button in Suggested Replies Template is disabled or not visible")
+            return
+        }
+        specificButton.tap()
+        let suggestedRepliesResponse = app.tables[AppScreen.innerChatScreenTableView]
+            .textViews[RichMessageResponseText.suggestedButtonResponse]
+        waitFor(object: suggestedRepliesResponse) { $0.exists }
    }
 
     func testLinkbuttonTemplate() {
@@ -67,6 +80,8 @@ class KommunicateRichMessageUITests: XCTestCase {
         waitFor(object: app) { $0.exists }
         app.typeText(GroupData.typeText2) // typing message
         app.buttons[InAppButton.ConversationScreen.send].tap() // sending message in group
+        sleep(3) /// To wait for response
+        app.swipeUp() /// To get to bottom of the screen.
         let linkResponse = app.tables[AppScreen.innerChatScreenTableView]
             .textViews[RichMessageResponseText.linkButtonResponse]
         waitFor(object: linkResponse) { $0.exists }
@@ -82,6 +97,8 @@ class KommunicateRichMessageUITests: XCTestCase {
         waitFor(object: app) { $0.exists }
         app.typeText(GroupData.typeText3) // typing message
         app.buttons[InAppButton.ConversationScreen.send].tap() // sending message in group
+        sleep(3) /// To wait for response
+        app.swipeUp() /// To get to bottom of the screen.
         let submitFirstResponse = app.tables[AppScreen.innerChatScreenTableView]
             .textViews[RichMessageResponseText.submitFirstResponse]
         waitFor(object: submitFirstResponse) { $0.exists }
@@ -100,6 +117,8 @@ class KommunicateRichMessageUITests: XCTestCase {
         waitFor(object: app) { $0.exists }
         app.typeText(GroupData.typeText5) // typing message
         app.buttons[InAppButton.ConversationScreen.send].tap() // sending message in group
+        sleep(3) /// To wait for response
+        app.swipeUp() /// To get to bottom of the screen.
         let imageTemplateResponse = app.tables[AppScreen.innerChatScreenTableView]
             .staticTexts[RichMessageResponseText.imageResponse]
         waitFor(object: imageTemplateResponse) { $0.exists }
@@ -110,6 +129,9 @@ class KommunicateRichMessageUITests: XCTestCase {
         waitFor(object: app) { $0.exists }
         app.typeText(GroupData.typeText6) // typing message
         app.buttons[InAppButton.ConversationScreen.send].tap() // sending message in group
+        sleep(3)
+        app.swipeUp()
+
         let listResponse = app.tables[AppScreen.innerChatScreenTableView]
             .textViews[RichMessageResponseText.listTemplateResponse]
         waitFor(object: listResponse) { $0.exists }
@@ -125,14 +147,29 @@ class KommunicateRichMessageUITests: XCTestCase {
         waitFor(object: app) { $0.exists }
         app.typeText(GroupData.typeText7) // typing message
         app.buttons[InAppButton.ConversationScreen.send].tap() // sending message in group
+        sleep(3) /// To wait for response
+        app.swipeUp() /// To get to bottom of the screen.
         let singleCardResponse = app.tables[AppScreen.innerChatScreenTableView]
             .textViews[RichMessageResponseText.singleCardResponse]
         waitFor(object: singleCardResponse) { $0.exists }
-        guard app.tables[AppScreen.innerChatScreenTableView].staticTexts[RichMessageButtons.linkButton].isEnabled else {
-            XCTFail("Button in Single card template is disabled or not visible")
+        // Fetch all matching elements
+        let linkButtons = app.tables[AppScreen.innerChatScreenTableView].staticTexts.matching(identifier: RichMessageButtons.linkButton)
+            
+        // Ensure there's at least one element
+        guard linkButtons.count > 0 else {
+            XCTFail("No buttons found in Single card template")
             return
         }
-        app.tables[AppScreen.innerChatScreenTableView].staticTexts[RichMessageButtons.linkButton].tap()
+            
+        // Use element(boundBy:) to select a specific button, e.g., the first one
+        let specificLinkButton = linkButtons.element(boundBy: 0) // Change index as needed
+            
+        guard specificLinkButton.isEnabled else {
+            XCTFail("Selected button in Single card template is disabled or not visible")
+            return
+        }
+            
+        specificLinkButton.tap()
     }
 
     func testCardCarouselTemplate() {
@@ -140,6 +177,8 @@ class KommunicateRichMessageUITests: XCTestCase {
         waitFor(object: app) { $0.exists }
         app.typeText(GroupData.typeText8) // typing message
         app.buttons[InAppButton.ConversationScreen.send].tap() // sending message in group
+        sleep(3) /// To wait for response
+        app.swipeUp() /// To get to bottom of the screen.
         let cardCarouselResponse = app.tables[AppScreen.innerChatScreenTableView]
             .textViews[RichMessageResponseText.cardCarouselResponse]
         waitFor(object: cardCarouselResponse) { $0.exists }
@@ -148,16 +187,16 @@ class KommunicateRichMessageUITests: XCTestCase {
     private func login() {
         let path = Bundle(for: KommunicateRichMessageUITests.self).url(forResource: "Info", withExtension: "plist")
         let dict = NSDictionary(contentsOf: path!) as? [String: Any]
-        let userId = dict?[GroupData.fillUserId]
-        let password = dict?[GroupData.fillPassword]
+        let userId = GroupData.fillUserId
+        let password = GroupData.fillPassword
         XCUIApplication().tap()
         let elementsQuery = XCUIApplication().scrollViews.otherElements
         let userIdTextField = elementsQuery.textFields[AppTextFeild.userId]
         userIdTextField.tap()
-        userIdTextField.typeText(userId as! String)
+        userIdTextField.typeText(userId)
         let passwordSecureTextField = elementsQuery.secureTextFields[AppTextFeild.password]
         passwordSecureTextField.tap()
-        passwordSecureTextField.typeText(password as! String)
+        passwordSecureTextField.typeText(password)
         elementsQuery.buttons[InAppButton.LaunchScreen.getStarted].tap()
     }
 
@@ -166,14 +205,30 @@ class KommunicateRichMessageUITests: XCTestCase {
         let launchConversationButton = app.buttons[InAppButton.EditGroup.launch]
         waitFor(object: launchConversationButton) { $0.exists }
         launchConversationButton.tap()
-        let createConversationButton = app.navigationBars[AppScreen.myChatScreen]
-        waitFor(object: createConversationButton) { $0.exists }
-        createConversationButton.buttons[InAppButton.CreatingGroup.startNewIcon].tap()
-        let inputView = app.otherElements[AppScreen.chatBar].children(matching: .textView).matching(identifier: AppTextFeild.chatTextView).firstMatch
-        waitFor(object: inputView) { $0.exists }
-        inputView.tap()
-        inputView.tap()
-        inputView.tap()
+        sleep(3)
+        // Check if the specific screen is on top
+        let isScreenOnTop = app.navigationBars[AppScreen.myChatScreen].exists
+
+        if isScreenOnTop {
+            // Perform actions only if the screen is not on top
+            let createConversationButton = app.navigationBars[AppScreen.myChatScreen]
+            waitFor(object: createConversationButton) { $0.exists }
+            createConversationButton.buttons[InAppButton.CreatingGroup.startNewIcon].tap()
+
+            let inputView = app.otherElements[AppScreen.chatBar].children(matching: .textView).matching(identifier: AppTextFeild.chatTextView).firstMatch
+            waitFor(object: inputView) { $0.exists }
+            inputView.tap()
+            inputView.tap()
+            inputView.tap()
+        } else {
+            
+            let inputView = app.otherElements[AppScreen.chatBar].children(matching: .textView).matching(identifier: AppTextFeild.chatTextView).firstMatch
+            waitFor(object: inputView) { $0.exists }
+            inputView.tap()
+            inputView.tap()
+            inputView.tap()
+        }
+
         return app
     }
 
