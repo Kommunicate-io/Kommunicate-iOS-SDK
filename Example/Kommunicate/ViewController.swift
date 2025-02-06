9251b05dd41d428bae4544185fb93850715148f8
Abhijeet Ranjan <abhijeet.ranjan@kommunicate.io>
2025-02-06 14:52:42 +0530

    updated Example app activityIndicator

--- Example/Kommunicate/ViewController.swift
+++ Example/Kommunicate/ViewController.swift
@@ -11,7 +11,7 @@
     import UIKit
 
     class ViewController: UIViewController {
-        let activityIndicator = UIActivityIndicatorView(style: .gray)
+        let activityIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.medium)
 
         override func viewDidLoad() {
             super.viewDidLoad()
